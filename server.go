876bd71a183c2f742598dd25b15162d43597e027
Pieter Noordhuis <pcnoordhuis@gmail.com>
2014-01-30 14:43:08 -0800

    Pass interface index to handler
    This allows users to use this code on machines with multiple interfaces
    without having to configure firewall rules or address translation.

--- server.go
+++ server.go
@@ -1,52 +1,69 @@
 package dhcp4
 
 import (
-	"log"
 	"net"
+
+	"code.google.com/p/go.net/ipv4"
 )
 
 type Handler interface {
-	ServeDHCP(req Packet, msgType MessageType, options Options) Packet
+	ServeDHCP(ifindex int, req Packet, reqtype MessageType, options Options) Packet
 }
 
-type ReaderFromUDP interface {
-	ReadFromUDP(b []byte) (n int, addr *net.UDPAddr, err error)
-}
-
-type WriterToUDP interface {
-	WriteToUDP(b []byte, addr *net.UDPAddr) (n int, err error)
-}
-
-// Serve listens on the listen net.PacketConn, passes DHCP packets to handler and sends
-// the result to the respond PacketConn.
+// Serve takes a net.PacketConn that it uses for both reading and writing DHCP
+// packets. Every packet is passed to the handler, which can process it and
+// optionally return a response packet. A response packet is then written back
+// to the network.
 //
-// Listen and respond are separate to support devices (such as a router)
-// with multiple interfaces, since Go's net library doesn't currently support binding broadcast
-// listeners to a particular interface.  See Examples or https://code.google.com/p/go/issues/detail?id=6935 for more info.
+// To capture limited broadcast packets (sent to 255.255.255.255), you must
+// listen on a socket bound to IP_ADDRANY (0.0.0.0). This means that broadcast
+// packets sent to any interface on the system may be delivered to this socket.
+// The network interface index a DHCP packet was received on is passed to the
+// handler, which is responsible for filtering packets by network interface.
 //
-func Serve(listen ReaderFromUDP, respond WriterToUDP, handler Handler) error {
+// Response packets are sent via the same network interface their corresponding
+// request was received on.
+func Serve(conn net.PacketConn, handler Handler) error {
+	p := ipv4.NewPacketConn(conn)
+	if err := p.SetControlMessage(ipv4.FlagInterface, true); err != nil {
+		return err
+	}
+
 	buffer := make([]byte, 1500)
 	for {
-		n, addr, err := listen.ReadFromUDP(buffer)
+		n, cm, addr, err := p.ReadFrom(buffer)
 		if err != nil {
 			return err
 		}
+
 		if n < 240 { // Packet too small to be DHCP
 			continue
 		}
-		p := Packet(buffer[:n])
-		options := p.ParseOptions()
-		msgType := options[OptionDHCPMessageType]
-		if len(msgType) != 1 {
-			return nil
-		}
-		// TODO consider more packet validity checks
-		if res := handler.ServeDHCP(p, MessageType(msgType[0]), options); res != nil {
-			if addr.IP.Equal(net.IPv4zero) || p.Broadcast() { // If IP not available, broadcast
-				addr.IP = net.IPv4bcast
+
+		src := *addr.(*net.UDPAddr)
+		dst := src
+		req := Packet(buffer[:n])
+		options := req.ParseOptions()
+
+		reqtype := MessageType(0)
+		if t := options[OptionDHCPMessageType]; len(t) != 1 {
+			continue
+		} else {
+			reqtype = MessageType(t[0])
+			if reqtype < Discover || reqtype > Inform {
+				continue
 			}
-			if _, e := respond.WriteToUDP(res, addr); e != nil {
-				log.Println("Write Error:", e.Error())
+		}
+
+		// TODO consider more packet validity checks
+		if res := handler.ServeDHCP(cm.IfIndex, req, reqtype, options); res != nil {
+			if src.IP.Equal(net.IPv4zero) || req.Broadcast() { // If IP not available, broadcast
+				dst.IP = net.IPv4bcast
+			}
+
+			// Inherit control message for interface index
+			if _, err := p.WriteTo(res, cm, &dst); err != nil {
+				return err
 			}
 		}
 	}
@@ -56,10 +73,10 @@ func Serve(listen ReaderFromUDP, respond WriterToUDP, handler Handler) error {
 // and then calls Serve with handler to handle requests
 // on incoming packets.
 func ListenAndServe(handler Handler) error {
-	l, err := net.ListenUDP("udp4", &net.UDPAddr{Port: 67})
+	l, err := net.ListenPacket("udp4", ":67")
 	if err != nil {
 		return err
 	}
 	defer l.Close()
-	return Serve(l, l, handler)
+	return Serve(l, handler)
 }
