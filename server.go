e56fc6e3c72ab69531c4f6b885a79c64695c6603
Richard Warburton <richard@warburton.it>
2014-02-01 18:33:56 +1300

    Switch to saner single conn interface

--- server.go
+++ server.go
@@ -1,65 +1,84 @@
 package dhcp4
 
 import (
-	"log"
 	"net"
+	"strconv"
 )
 
 type Handler interface {
 	ServeDHCP(req Packet, msgType MessageType, options Options) Packet
 }
 
-type ReaderFromUDP interface {
-	ReadFromUDP(b []byte) (n int, addr *net.UDPAddr, err error)
+// ServeConn is the bare minimum connection functions required by Serve()
+// It allows you to create custom connections for greater control,
+// such as ServeIfConn (see serverif.go), which locks to a given interface.
+type ServeConn interface {
+	ReadFrom(b []byte) (n int, addr net.Addr, err error)
+	WriteTo(b []byte, addr net.Addr) (n int, err error)
 }
 
-type WriterToUDP interface {
-	WriteToUDP(b []byte, addr *net.UDPAddr) (n int, err error)
-}
-
-// Serve listens on the listen net.PacketConn, passes DHCP packets to handler and sends
-// the result to the respond PacketConn.
+// Serve takes a ServeConn (such as a net.PacketConn) that it uses for both
+// reading and writing DHCP packets. Every packet is passed to the handler,
+// which processes it and optionally return a response packet for writing back
+// to the network.
 //
-// Listen and respond are separate to support devices (such as a router)
-// with multiple interfaces, since Go's net library doesn't currently support binding broadcast
-// listeners to a particular interface.  See Examples or https://code.google.com/p/go/issues/detail?id=6935 for more info.
+// To capture limited broadcast packets (sent to 255.255.255.255), you must
+// listen on a socket bound to IP_ADDRANY (0.0.0.0). This means that broadcast
+// packets sent to any interface on the system may be delivered to this
+// socket.  See: https://code.google.com/p/go/issues/detail?id=7106
 //
-func Serve(listen ReaderFromUDP, respond WriterToUDP, handler Handler) error {
+// Additionally, response packets may not return to the same
+// interface that the request was received from.  Writing a custom ServeConn,
+// or using ServeIf() can provide a workaround to this problem.
+func Serve(conn ServeConn, handler Handler) error {
 	buffer := make([]byte, 1500)
 	for {
-		n, addr, err := listen.ReadFromUDP(buffer)
+		n, addr, err := conn.ReadFrom(buffer)
 		if err != nil {
 			return err
 		}
 		if n < 240 { // Packet too small to be DHCP
 			continue
 		}
-		p := Packet(buffer[:n])
-		options := p.ParseOptions()
-		msgType := options[OptionDHCPMessageType]
-		if len(msgType) != 1 {
-			return nil
+		req := Packet(buffer[:n])
+		if req.HLen() > 16 { // Invalid size
+			continue
 		}
-		// TODO consider more packet validity checks
-		if res := handler.ServeDHCP(p, MessageType(msgType[0]), options); res != nil {
-			if addr.IP.Equal(net.IPv4zero) || p.Broadcast() { // If IP not available, broadcast
-				addr.IP = net.IPv4bcast
+		options := req.ParseOptions()
+		var reqType MessageType
+		if t := options[OptionDHCPMessageType]; len(t) != 1 {
+			continue
+		} else {
+			reqType = MessageType(t[0])
+			if reqType < Discover || reqType > Inform {
+				continue
 			}
-			if _, e := respond.WriteToUDP(res, addr); e != nil {
-				log.Println("Write Error:", e.Error())
+		}
+		if res := handler.ServeDHCP(req, reqType, options); res != nil {
+			// If IP not available, broadcast
+			ipStr, portStr, err := net.SplitHostPort(addr.String())
+			if err != nil {
+				return err
+			}
+
+			if net.ParseIP(ipStr).Equal(net.IPv4zero) || req.Broadcast() {
+				port, _ := strconv.Atoi(portStr)
+				addr = &net.UDPAddr{IP: net.IPv4bcast, Port: port}
+			}
+			if _, e := conn.WriteTo(res, addr); e != nil {
+				return e
 			}
 		}
 	}
 }
 
-// ListenAndServe listens on the UDP network address addr
-// and then calls Serve with handler to handle requests
-// on incoming packets.
+// ListenAndServe listens on the UDP network address addr and then calls
+// Serve with handler to handle requests on incoming packets.
 func ListenAndServe(handler Handler) error {
-	l, err := net.ListenUDP("udp4", &net.UDPAddr{Port: 67})
+	l, err := net.ListenPacket("udp4", ":67")
 	if err != nil {
 		return err
 	}
 	defer l.Close()
-	return Serve(l, l, handler)
+	return Serve(l, handler)
 }
